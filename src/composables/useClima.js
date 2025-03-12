351628ef582d5c7d3890f5575222ac3daaf915da
Roberto Carlos Torres Galvan <20002873@alumnos.utleon.edu.mx>
2025-03-12 13:22:10 -0600

    cambio de https en url

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -13,7 +13,7 @@ export default function useClima() {
         error.value = '';
         const key = import.meta.env.VITE_API_KEY;
         try {
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${busqueda.ciudad},${busqueda.pais}&limit=1&appid=${key}`;
+            const url = `https://api.openweathermap.org/geo/1.0/direct?q=${busqueda.ciudad},${busqueda.pais}&limit=1&appid=${key}`;
             const { data } = await axios(url);
             const { lat, lon } = data[0];
             const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
